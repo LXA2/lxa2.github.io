33aa3d2803e62d8a262cadd22345381eb461a0f6
LXA2 <106149641+LXA2@users.noreply.github.com>
2022-12-30 18:30:26 +0800

    a

--- kwm/js/settings.js
+++ kwm/js/settings.js
@@ -1,15 +1,16 @@
-function settings(){
-	var a=document.getElementById("settings");
-	a.style.marginLeft="0";//35
-	a.style.marginTop="0";//10
-	a.style.opacity="0.9";
-	a.style.borderRadius="1.5em";
-	a.style.width="32em";
+function settings() {
+	var a = document.getElementById("settings");
+	a.style.marginLeft = "0"; //35
+	a.style.marginTop = "0"; //10
+	a.style.opacity = "0.9";
+	a.style.borderRadius = "1.5em";
+	a.style.width = "32em";
 }
-function setEsc(){
-	var a=document.getElementById("settings");
-	a.style.marginLeft="-35em";//35
-	a.style.marginTop="-10em";//10
-	a.style.opacity="0";
-	a.style.borderRadius="12em";
+
+function setEsc() {
+	var a = document.getElementById("settings");
+	a.style.marginLeft = "-35em"; //35
+	a.style.marginTop = "-10em"; //10
+	a.style.opacity = "0";
+	a.style.borderRadius = "12em";
 }
