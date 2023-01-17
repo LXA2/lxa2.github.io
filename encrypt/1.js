3887c35aa77773858cd65662ccffea19a8ff5419
LXA2 <106149641+LXA2@users.noreply.github.com>
2023-01-17 14:03:47 +0800

    a

--- encrypt/1.js
+++ encrypt/1.js
@@ -29,17 +29,17 @@ function pcCopy1() {
   copyText.setSelectionRange(0, 99999); 
   navigator.clipboard.writeText(copyText.value);
 }
-function toUnicode(theString) {
-  var unicodeString = '';
-  for (var i = 0; i < theString.length; i++) {
-    var theUnicode = theString.charCodeAt(i).toString(16);
-    while (theUnicode.length < 4) {
-      theUnicode = '0' + theUnicode;
-    }
-    unicodeString += theUnicode;
-  }
-  return unicodeString;
-}
+// function toUnicode(theString) {
+//   var unicodeString = '';
+//   for (var i = 0; i < theString.length; i++) {
+//     var theUnicode = theString.charCodeAt(i).toString(16);
+//     while (theUnicode.length < 4) {
+//       theUnicode = '0' + theUnicode;
+//     }
+//     unicodeString += theUnicode;
+//   }
+//   return unicodeString;
+// }
 function randomNum(min,max){ 
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 } 
@@ -141,36 +141,36 @@ function generatePrivateKey(){
 
 
 
-var a2=0;
-function generatePrivateKey_nonono(){
-	var ua=navigator.userAgent;
-	var a1=toUnicode(ua);
-	// if (a1.length>=188){
-	// 	a1=a1.substring(60,188);
-	// }
-	// else{
-	// 	while (a1.length<188) {
-	// 		a1=a1+a1;
-	// 	}
-	// 	a1=a1.substring(60,188);
-	// }
-	a2=0;
-	while (a2==0){
-		a2=Math.floor((new Date().getTime()*new Date().getSeconds()+Math.sqrt(new Date().getSeconds()*new Date().getMinutes()))*100);
-	}
-	console.log(a2.length);
-	for (var i = 0; i < a2.length; i++) {
-		console.log(a2[i]);
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	console.log(a1);
-	document.getElementById("pcPubKeyCopy").value=a1;
-}
+// var a2=0;
+// function generatePrivateKey_nonono(){
+// 	var ua=navigator.userAgent;
+// 	var a1=toUnicode(ua);
+// 	// if (a1.length>=188){
+// 	// 	a1=a1.substring(60,188);
+// 	// }
+// 	// else{
+// 	// 	while (a1.length<188) {
+// 	// 		a1=a1+a1;
+// 	// 	}
+// 	// 	a1=a1.substring(60,188);
+// 	// }
+// 	a2=0;
+// 	while (a2==0){
+// 		a2=Math.floor((new Date().getTime()*new Date().getSeconds()+Math.sqrt(new Date().getSeconds()*new Date().getMinutes()))*100);
+// 	}
+// 	console.log(a2.length);
+// 	for (var i = 0; i < a2.length; i++) {
+// 		console.log(a2[i]);
+// 	}
+	// 
+	// 
+	// 
+	// 
+	// 
+	// 
+	// 
+	// 
+// 	console.log(a1);
+// 	document.getElementById("pcPubKeyCopy").value=a1;
+// }
 
--- encrypt/adduser.js
+++ encrypt/adduser.js
@@ -1,13 +1,16 @@
 var strength=0;
 let user=Object();
-if (localstorage=="") {
-    
+if (localStorage.getItem("encryptUser")==null) {
+    user.u1=Object();
+    user.u1.name="默认用户"+new Date().getUTCDate;
+    user.u1.strength=2;
+    alert("aaa")
+    localStorage.setItem("encryptUser",JSON.stringify(user));
 } else {
-    
+    user=JSON.parse(localStorage.getItem("encryptUser"));
+    console.log(user);
 }
-user.u1=Object();
-u1.name="默认用户"+new Date().getUTCDate;
-u1.strength=2;
+
 function adduser(a){
     switch (a) {
         case 1:
@@ -15,7 +18,7 @@ function adduser(a){
             document.getElementById("addUserQuery").innerHTML=`<p>
             用户名称
         </p>
-        <input type="text" id="userName" style="height: 1.5em;width: 30em;font-size: 0.8em;">
+        <input type="text" id="userName" style="height: 1.5em;width: 30em;font-size: 0.8em;" value="">
         <hr>
         <p>
             加密强度（强加密运算时间可能过长）
@@ -34,9 +37,11 @@ function adduser(a){
             break;
     
         case 2:
-            if (document.getElementById("userName").ariaValueMax.length!=0) {
+            if (document.getElementById("userName").) {
                 if (strength!=0) {
-                    
+                    user[`${user.length+1}`]=Object();
+                    user[`${user.length}`][]
+                    document.getElementById("addUserQuery").innerHTML="";
                 } else {
                     alert("请选择加密强度");
                 }
