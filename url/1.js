c2272371562449735717cb04eca1297bacdae21f
LXA2 <106149641+LXA2@users.noreply.github.com>
2023-01-20 19:46:56 +0800

    a

--- url/1.js
+++ url/1.js
@@ -1,4 +1,5 @@
 //document.getElementById("body").innerHTML=``;
+var version=1;
 function copy1(){
     var copyText = document.getElementById("res1");
     copyText.select();
@@ -12,10 +13,53 @@ function copy2(){
     navigator.clipboard.writeText(copyText.value);
 }
 function url(a){
-    var res,b;
-    
-    res=`https://lxa2.github.io/url/index.html?`
+    var b,c;
+	b=a.charCodeAt(0).toString();
+    for (var i = 1; i < (a.length-1); i++) {
+		c=a.charCodeAt(i);
+		while (c.length<6){
+			c="0"+c.toString();
+		}
+		console.log(i);
+    	b=b+c;
+    }
+	b=BigInt(b);
+	console.log(b);
+	var v=version.toBase();
+	while (v.length<2){
+		console.log()
+	}
+	b=b.toString(36);
+	document.getElementById("res1").value=`https://lxa2.github.io/url/index.html?${b}`;
 }
 function text(a){
     a;
+}
+
+
+
+
+
+
+
+
+const base = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const ben = base.length;
+
+function toBase(num) {
+    let arr = [];
+    while (num > 0) {
+        arr.push(base[num % ben]);
+        num = Math.floor(num / ben);
+    }
+    return arr.reverse().join('');
+}
+function to10(baseNum) {
+    baseNum = baseNum.split('').reverse().join('');
+    let val = 0;
+    for (let i = 0; i < baseNum.length; i++) {
+        let c = baseNum[i];
+        val += (base.indexOf(c) * Math.pow(ben, i));
+    }
+    return val;
 }
