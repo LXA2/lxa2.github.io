b1fdd50c9f02bf34ecd9052d05b59a8cccbf91e2
LXA2 <106149641+LXA2@users.noreply.github.com>
2023-01-30 17:06:29 +0800

    Update 1.js

--- url/1.js
+++ url/1.js
@@ -3,6 +3,7 @@ let u=Object();
 let t=Object();
 u.blank="";u.auto="";u.startTime="";u.endTime="";u.key="";u.t1=Object();u.t2=Object();
 t.blank="";t.startTime="";t.endTime="";t.key="";t.t1=Object();t.t2=Object();
+
 // u.startTime=Object();u.endTime=Object();u.key=Object();
 // u.startTime.year="";u.startTime.month="";u.startTime.year=""
 function copy1(){
