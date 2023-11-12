922be4eb7c0b83285bb4d03c5a70b2acd653119c
LXA2 <106149641+LXA2@users.noreply.github.com>
2023-11-12 19:45:28 +0800

    Update 1.js

--- khlnb/js/1.js
+++ khlnb/js/1.js
@@ -1,3 +1,19 @@
 function video(){
     document.getElementById("bd").innerHTML="hahahahahahahahaha";
 }
+function iframe() {
+  url="https://space.bilibili.com/347050218";
+  var bodyElement = document.getElementById("bd");
+  bodyElement.innerHTML = "";  // 清空body元素的内容
+  
+  var iframeElement = document.createElement("iframe");
+  iframeElement.style.border = "none";  // 设置iframe无边框
+  iframeElement.style.position = "fixed";
+  iframeElement.style.top = "0";
+  iframeElement.style.left = "0";
+  iframeElement.style.width = "100%";
+  iframeElement.style.height = "100%";
+  iframeElement.src = url;  // 设置iframe的URL
+  
+  bodyElement.appendChild(iframeElement);  // 将iframe添加到body元素中
+}
