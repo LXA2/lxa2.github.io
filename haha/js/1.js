fbecbe6a1db7486ec56567f21984f45b93544b52
LXA2 <106149641+LXA2@users.noreply.github.com>
2023-12-31 15:50:21 +0800

    Create 1.js

---
+++ haha/js/1.js
@@ -0,0 +1,18 @@
+function video(){
+    document.getElementById("bd").innerHTML="hahahahahahahahaha";
+}
+function iframe() {
+  url="https://space.bilibili.com/1704008580";
+  var bodyElement = document.getElementById("bd");
+  bodyElement.innerHTML = "<div class='title3'>KHLNB</div><div class='title4'>ZJW!!!!!!</div>"; 
+  var iframeElement = document.createElement("iframe");
+  iframeElement.style.border = "none"; 
+  iframeElement.style.position = "fixed";
+  iframeElement.style.top = "0";
+  iframeElement.style.left = "0";
+  iframeElement.style.width = "100%";
+  iframeElement.style.height = "100%";
+  iframeElement.src = url;  
+  
+  bodyElement.appendChild(iframeElement); 
+}
